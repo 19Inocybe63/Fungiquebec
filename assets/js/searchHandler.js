d5ee2d619c62bb137273ffc78d533cf8d31d79c6
Jean-Francois L'Heureux <jflheureux@coveo.com>
2017-03-13 20:16:51 -0400

    + Sexyness de la recherche

--- assets/js/searchHandler.js
+++ assets/js/searchHandler.js
@@ -11,41 +11,9 @@ var searchHandler = {
         if (query) {
             this.query = query;
         }
-        // for (var i = 0; i < this.facets.length; ++i) {
-        //     this.createFacet(i, this.facets[i]);
-        // }
 
         this.search();
     },
-    // createFacet: function(index, facet) {
-    //     var _this = this;
-    //     var facetElement = $("<div>").addClass("facet");
-    //     facetElement.append($("<div>").addClass("facetName").text(facet.label));
-    //     facetElement.append($("<a>")
-    //                 .addClass("facetSelectedValue")
-    //                 .attr("href", "#")
-    //                 .data("index", index)
-    //                 .on("click", function(e) {
-    //                     _this.onFacetSelectedValueClicked(e, _this);
-    //                 }));
-    //     facetElement.append($("<div>")
-    //                 .addClass("facetValues"));
-
-    //     this.facetsContainer.append(facetElement);
-    //     this.facets[index].element = $($('.facet')[index]);
-    // },
-    // onFacetSelectedValueClicked: function(e, _this) {
-    //     var facetSelectedValueElement = $(e.target);
-    //     facetSelectedValueElement.text("");
-
-    //     var index = facetSelectedValueElement.data("index");
-    //     _this.facets[index].selectedValue = undefined;
-    //     _this.search();
-
-    //     var facetElement = _this.facets[index].element;
-    //     var facetValuesElement = facetElement.find('.facetValues');
-    //     facetValuesElement.show();
-    // },
     search: function () {
         this.hideAllResults();
 
@@ -98,76 +66,6 @@ var searchHandler = {
         var facetResults = this.updateFacetsFromSearchResults(searchResults);
         this.showResults(searchResults, facetResults);
     },
-    // updateFacetsFromAllResults: function() {
-    //     var _this = this;
-
-    //     for (var i = 0; i < this.facets.length; ++i) {
-    //         var facet = this.facets[i];
-
-    //         if (facet.selectedValue) {
-    //             // Do not touch facets with selection
-    //             continue;
-    //         }
-
-    //         // Clear the DOM
-    //         var facetElement = facet.element;
-    //         var facetValuesElement = facetElement.find('.facetValues');
-    //         // var facetValueElements = facetValuesElement.find('.facetValue');
-    //         // for (var e = 0; e < facetValueElements.length; ++e) {
-    //         //     facetValuesElement.children.remove(facetValueElements[i]);
-    //         // }
-    //         facetValuesElement.html("");
-
-    //         // Find all the values for this facet
-    //         var field = facet.field;
-    //         var facetValues = [];
-
-    //         for (var r = 0; r < this.allResults.length; ++r) {
-    //             var fieldValue = this.allResults[r][field];
-    //             if (!fieldValue) {
-    //                 continue;
-    //             }
-    //             facetValues.push(fieldValue);
-    //         }
-
-    //         if (facetValues.length > 0) {
-    //             // Sort the values and compute their occurences
-    //             var uniqueValues = Array.from(facetValues);
-    //             var numberOfUniqueValues = 0;
-    //             while (numberOfUniqueValues != uniqueValues.length) {
-    //                 numberOfUniqueValues = uniqueValues.length;
-    //                 $.unique(uniqueValues);
-    //             }
-    //             uniqueValues.sort();
-
-    //             var facetValuesOccurrences = uniqueValues.map(function (uniqueValue, index) {
-    //                 return {
-    //                     value: uniqueValue,
-    //                     count: facetValues.filter(function(value) {
-    //                         return value == uniqueValue;
-    //                     }).length
-    //                 };
-    //             });
-
-    //             // Add the values to the DOM
-    //             for (var v = 0; v < facetValuesOccurrences.length; ++v) {
-    //                 facetValuesElement.append($("<a>")
-    //                                   .addClass("facetValue")
-    //                                   .attr("href", "#")
-    //                                   .data("index", i)
-    //                                   .text(facetValuesOccurrences[v].value + " (" + facetValuesOccurrences[v].count + ")")
-    //                                   .on("click", function(e) {
-    //                                       _this.onFacetValueClicked(e, _this);
-    //                                   }));
-    //             }
-
-    //             // Ensure the facet is displayed
-    //             facetElement.show();
-    //         } else {
-    //             facetElement.hide();
-    //         }
-    //     }
-    // },
     updateFacetsFromSearchResults: function(results) {
         var _this = this;
         var facetResults = [];
@@ -218,21 +116,4 @@ var searchHandler = {
 
         return facetResults;
     }
-    // onFacetValueClicked: function(e, _this) {
-    //     var facetValueElement = $(e.target);
-    //     var value = facetValueElement.text();
-    //     var indexToTrim = value.indexOf('(') - 1;
-    //     value = value.substr(0, indexToTrim);
-
-    //     var index = facetValueElement.data("index");
-    //     _this.facets[index].selectedValue = value;
-
-    //     var facetElement = _this.facets[index].element;
-    //     var facetValuesElement = facetElement.find('.facetValues');
-    //     facetValuesElement.hide();
-    //     var facetSelectedValueElement = facetElement.find('.facetSelectedValue');
-    //     facetSelectedValueElement.text(value);
-
-    //     _this.search();
-    // }
 };

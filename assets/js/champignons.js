d125e6d2afad5561672c0ebe800d6573ce5d2a2c
Claude Kaufholtz-Couture <kaufholtz@videotron.ca>
2017-05-21 17:30:20 -0400

    Strip HTML dans les valeurs de l'index

--- assets/js/champignons.js
+++ assets/js/champignons.js
@@ -83,11 +83,11 @@ var champsChampignons = [
 var champignons = [
     {% for champignon in champignonsTries %}  {
         id: {{ forloop.index }},
-        nom: "{{ champignon.nom }}",
-        nom_francais: "{{ champignon.details.nom-francais }}",
+        nom: "{{ champignon.nom | strip_html }}",
+        nom_francais: "{{ champignon.details.nom-francais | strip_html }}",
         url: "{{ champignon.url | prepend: site.baseurl }}",
         {% for champ in site.champs-details %}{% if champ.search != "exclure" and champignon.details[champ.name] %}
-        {{ champ.name | replace: "-", "_" }}: "{{ champignon.details[champ.name] | replace: '"', ' ' }}",
+        {{ champ.name | replace: "-", "_" }}: "{{ champignon.details[champ.name] | replace: '"', ' ' | strip_html }}",
         {% endif %}{% endfor %}
 
         {% for champ in site.champs-collections %}{% if champ.search != "exclure" %}
@@ -95,13 +95,13 @@ var champignons = [
                 {% for sousChamp in champ.champs %}{% if sousChamp.search != "exclure" %}
                     {% capture valeurSousChamp %}{% for element in champignon.collections %}{% if element[champ.name] %}{% if element[champ.name][sousChamp.name] %}{{ element[champ.name][sousChamp.name] }} {% endif %}{% endif %}{% endfor %}{% endcapture %}
                     {% if valeurSousChamp %}
-        {{ champ.name | replace: "-", "_" }}_{{ sousChamp.name | replace: "-", "_" }}: "{{ valeurSousChamp | replace: '"', ' ' }}",
+        {{ champ.name | replace: "-", "_" }}_{{ sousChamp.name | replace: "-", "_" }}: "{{ valeurSousChamp | replace: '"', ' ' | strip_html }}",
                     {% endif %}
                 {% endif %}{% endfor %}
             {% else %}
                 {% capture valeurChamp %}{% for element in champignon.collections %}{% if element[champ.name] %}{{ element[champ.name] }} {% endif %}{% endfor %}{% endcapture %}
                 {% if valeurChamp %}
-        {{ champ.name | replace: "-", "_" }}: "{{ valeurChamp | replace: '"', ' ' }}",
+        {{ champ.name | replace: "-", "_" }}: "{{ valeurChamp | replace: '"', ' ' | strip_html }}",
                 {% endif %}
             {% endif %}
         {% endif %}{% endfor %}
@@ -109,7 +109,7 @@ var champignons = [
         {% for champ in site.champs-description %}{% if champ.search != "exclure" and champ.name != "cristaux apicaux" and champignon.description[champ.name] %}
             {% assign champSplit = champignon.description[champ.name] | strip | newline_to_br | split: "<br />" %}
             {% capture valeurChamp %}{% for ligne in champSplit %}{{ ligne | strip | replace: '"', ' ' }} {% endfor %}{% endcapture %}
-        {{ champ.name | replace: "-", "_" }}: "{{ valeurChamp }}",
+        {{ champ.name | replace: "-", "_" }}: "{{ valeurChamp | strip_html }}",
         {% endif %}{% endfor %}
     },
     {% endfor %}
